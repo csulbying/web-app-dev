c9e471c8463e0137a6bb71e5f0407a15b78bf875
ying.liu <yingliu@galaxys-MacBook-Pro.local>
2019-09-26 21:06:01 -0700

    refactor add numbers

--- code-samples/02-add-numbers/app.js
+++ code-samples/02-add-numbers/app.js
@@ -1,18 +1,18 @@
 const addBtn = document.getElementById('addBtn')
+const num1Input = document.getElementById('num1')
+const num2Input = document.getElementById('num2')
+const output = document.getElementById('output')
 
-addBtn.addEventListener('click', addNumbers)
+addBtn.addEventListener('click', addNumber)
 
 function addNumbers() {
-  const num1Input = document.getElementById('num1')
   const num1String = num1Input.value
   const num1Value = parseInt(num1String, 10)
 
-  const num2Input = document.getElementById('num2')
   const num2String = num2Input.value
   const num2Value = parseInt(num2String, 10)
 
   const sum = num1Value + num2Value
-  const output = document.getElementById('output')
 
   output.innerHTML = sum
 }
