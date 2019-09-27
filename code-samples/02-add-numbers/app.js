e25074cc1297c9f3dd91a6beeba835294b1c9e64
ying.liu <yingliu@galaxys-MacBook-Pro.local>
2019-09-26 20:04:43 -0700

    revise add numbers sample

--- code-samples/02-add-numbers/app.js
+++ code-samples/02-add-numbers/app.js
@@ -1,13 +1,18 @@
 const addBtn = document.getElementById('addBtn')
-const num1 = document.getElementById('num1')
-const num2 = document.getElementById('num2')
-const output = document.getElementById('output')
 
 addBtn.addEventListener('click', addNumbers)
 
 function addNumbers() {
-  const val1 = num1.value
-  const val2 = num2.value
-  const sum = parseInt(val1, 16) + parseInt(val2, 16)
+  const num1Input = document.getElementById('num1')
+  const num1String = num1Input.value
+  const num1Value = parseInt(num1String, 10)
+
+  const num2Input = document.getElementById('num2')
+  const num2String = num2Input.value
+  const num2Value = parseInt(num2String, 10)
+
+  const sum = num1Value + num2Value
+  const output = document.getElementById('output')
+
   output.innerHTML = sum
 }
