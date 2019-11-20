fd530f030816a351ecae485cbb848b1c359aacab
yingliu <yingliu4203@gmail.com>
2019-11-20 11:20:11 -0800

    update firebase code

--- 31-firebase-crud-demo/source-code/firebase/public/app.js
+++ 31-firebase-crud-demo/source-code/firebase/public/app.js
@@ -1,24 +1,40 @@
-const form = document.getElementById('leave-message')
-const input = document.getElementById('message')
+const dbCollection = firebase.firestore().collection('todos')
 
-form.addEventListener('submit', e => {
-  // Prevent the default form redirect
-  e.preventDefault()
-  // Write a new message to the database collection "guestbook"
+const input = document.getElementById('myInput')
+const addBtn = document.getElementById('addBtn')
+const todoList = document.getElementById('myUL')
 
-  addData()
-  // clear message input field
-  input.value = ''
-  // Return false to avoid redirect
-  return false
+const ENTER_KEY_CODE = 13
+input.addEventListener('keyup', event => {
+  if (event.keyCode === ENTER_KEY_CODE) {
+    appendTodo()
+  }
 })
 
-function addData() {
-  firebase
-    .firestore()
-    .collection('guestbook')
+addBtn.addEventListener('click', appendTodo)
+
+todoList.addEventListener('click', handleTodoClick)
+
+dbCollection.onSnapshot(docs => {
+  todoList.innerHTML = ''
+  docs.forEach(doc => createItemFromData(doc))
+})
+
+function appendTodo() {
+  const inputValue = input.value
+  if (inputValue === '') {
+    alert('Todo cannot be empty')
+  } else {
+    input.value = ''
+    saveTodo(inputValue)
+  }
+}
+
+function saveTodo(todoText) {
+  dbCollection
     .add({
-      text: input.value,
+      task: todoText,
+      checked: false,
       timestamp: Date.now(),
     })
     .then(function(docRef) {
@@ -27,4 +43,58 @@ function addData() {
     .catch(function(error) {
       console.error('Error adding document: ', error)
     })
+}
+
+function createItemFromData(doc) {
+  const refid = doc.id
+  const data = doc.data()
+  const todoItem = document.createElement('li')
+
+  // set ref id
+  todoItem.setAttribute('data-refid', refid)
+
+  // save and set the checked class
+  if (data.checked) {
+    todoItem.setAttribute('data-checked', 'checked')
+    todoItem.classList.add('checked')
+  }
+
+  // the todo text content
+  const todo = document.createTextNode(data.task)
+  todoItem.appendChild(todo)
+
+  // the close symbol
+  const span = document.createElement('SPAN')
+  const txt = document.createTextNode('X')
+  span.className = 'close'
+  span.appendChild(txt)
+  todoItem.appendChild(span)
+
+  todoList.appendChild(todoItem)
+}
+
+function handleTodoClick(event) {
+  const target = event.target
+  if (target.tagName === 'LI') {
+    toggleChecked(target)
+  } else if (target.tagName === 'SPAN') {
+    const item = target.parentElement
+    removeItem(item)
+  }
+}
+
+function toggleChecked(listItem) {
+  const refid = listItem.dataset.refid
+  const docRef = dbCollection.doc(refid)
+  if (listItem.dataset.checked === 'checked') {
+    docRef.update({ checked: false })
+  } else {
+    docRef.update({ checked: true })
+  }
+}
+
+function removeItem(listItem) {
+  const refid = listItem.dataset.refid
+  const docRef = dbCollection.doc(refid)
+  docRef.delete()
 }
